fdf89ae9aedda0c3ff27897a9e1b07631c481eeb
cody <cody18v@gmail.com>
2021-04-14 08:01:29 -0700

    test 123

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php
-
-echo __DIR__;
+<div class="landing">
+    <div class="landing__content">
+        <h1 class="landing__title">
+            با کمترین هزینه و سریع ترین زمان محصول <br />
+            خودتو سفارش بده و با استفاده ازش لذت ببر
+        </h1>
+    </div>
+</div>
