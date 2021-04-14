b21344169500b91fc7090fd2a70fb6a970034eec
cody <cody18v@gmail.com>
2021-04-14 08:53:29 -0700

    test 123457891011121314151617189

--- test.php
+++ test.php
@@ -2,7 +2,12 @@
 
 require_once realpath(__DIR__) . '/app/functions/connect.php';
 
+var_dump("ONE");
+
 $results = $connection->query("SELECT * FROM topics");
+var_dump("TWO");
 $results->execute();
+var_dump("THREE");
 $topics = $results->fetchAll(PDO::FETCH_OBJ);
+var_dump("FOUR");
 var_dump($topics);
