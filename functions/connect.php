4bff7f7cb41939aa8b40ccf3deaed7393da6f36d
cody <cody18v@gmail.com>
2021-04-14 07:58:42 -0700

    test 12

--- functions/connect.php
+++ functions/connect.php
@@ -1,5 +1,5 @@
 <?php
 
 
-$connection = new PDO("mysql:host=localhost;dbname=eshop;", "root", "");
+$connection = new PDO("mysql:host=sql4.freesqldatabase.com;dbname=sql4405633;", "sql4405633", "CedfX8J5JS");
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
