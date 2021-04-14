6620799377210d1091857e7529adb84d59e14f8b
cody <cody18v@gmail.com>
2021-04-14 10:19:35 -0700

    hosein keramatihfbyfgf555555555556555256665554556555664555555555hdygddufyf5dd

--- functions/single.php
+++ functions/single.php
@@ -10,7 +10,7 @@ if (!isset($_GET['id'])) {
 
 $id = $_GET['id'];
 // $sql_command = "SELECT * FROM Products p JOIN Topics t ON p.topicId=t.id WHERE p.id = '$id'";
-$sql_command = "SELECT * FROM Products INNER JOIN Topics ON Products.topicId = Topics.id WHERE Products.id = '$id'";
+$sql_command = "SELECT * FROM Products WHERE Products.id = '$id'";
 var_dump($sql_command);
 $productStatement = $connection->query($sql_command);
 var_dump($productStatement);
